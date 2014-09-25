ca181661daa5ca42e7c5e9d7750fa547fa0222f4
djhi <gildas.garcia@gmail.com>
2014-09-25 18:17:42 +0200

    added double quotes around strings in the profile.d scripts

--- .profile.d/mongo.sh
+++ .profile.d/mongo.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 echo "Setting up MONGO_URL in .profile.d";
-export MONGO_URL=${MONGO_URL:-$MONGOHQ_URL};
+export MONGO_URL="${MONGO_URL:-$MONGOHQ_URL}";
 
 echo "${MONGO_URL}";
